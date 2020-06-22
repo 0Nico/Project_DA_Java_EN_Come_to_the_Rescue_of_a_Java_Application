fe4a01cd40fcd84b0e4a97acbbd92b36d51d3e2b
Nicolas Dupuy <nico@me.fr>
2020-06-22 19:57:19 +0200

    Add javadoc on main analytic

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,18 +4,29 @@ package com.hemebiotech.analytics;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * The main class software with user interactions  
+ *@author 0Nico
+ *@version 1.4
+ */
 public class AnalyticsCounter {
 	
 	public static void main(String args[]) {
 		
+		/**  Instance of the analyze tool  */
 		SymptomToolsBox toolsbox = new SymptomToolsBox();
+		
+		/** Location of the file to analyze */
 		String filePath;
+		
+		/** Future location of the software result */
 		String resultPath;
 		
 		System.out.println("** Welcome to the Symptoms Analytics Counter Software v1.1 **");
 		System.out.println("** Developped by HemeBioTech - 2020 . All right reserved. ** \n\n");
 		System.out.println("Please enter the absolute location of your symptoms's files. One symptom per line, with no empty lines (means the end of the file). \n ");
 		
+		/** Launch of the analyze */
 		Scanner input = new Scanner(System.in);	    
 	    filePath = input.next();
 		try {
@@ -25,7 +36,7 @@ public class AnalyticsCounter {
 			e.printStackTrace();
 		}
 		
-		
+		/** Launch of the last step - writing results */		
 		System.out.println("Please enter the name of the result file (will overwrite if file already exist).\n");
 		resultPath = input.next();
 		try {
