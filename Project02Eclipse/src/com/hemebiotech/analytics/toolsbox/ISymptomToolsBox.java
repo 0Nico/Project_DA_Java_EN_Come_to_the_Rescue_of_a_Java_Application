df0ea3e67ea279e139e546ec408a68195d0765ae
Nicolas Dupuy <nicolas.dupuy@mimbus.com>
2020-07-02 12:05:12 +0200

    Fourth software correction

--- Project02Eclipse/src/com/hemebiotech/analytics/toolsbox/ISymptomToolsBox.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/toolsbox/ISymptomToolsBox.java
@@ -4,24 +4,35 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 
+import com.hemebiotech.analytics.toolsbox.exceptions.CreateFileException;
+import com.hemebiotech.analytics.toolsbox.exceptions.ReadFileException;
+import com.hemebiotech.analytics.toolsbox.exceptions.ReadLinesException;
+import com.hemebiotech.analytics.toolsbox.exceptions.WriteLineException;
 import com.hemebiotech.analytics.toolsbox.options.OrderingOption;
 
 public interface ISymptomToolsBox {
 
 	
 	/**
-	 * Reading method, come in first order to execute correctly the full software.
+	 * Read line by line to collect and count file's symptoms
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 * @throws FileNotFoundException if the file path is incorrect & RuntimeException if the lines aren't readable
 	 * @return A map who contains symptoms extract and count from the file
+	 * @throws ReadFileException 
+	 * @throws ReadLinesException 
+	 * @throws IOException 
 	 */
 	public Map<String, Integer> readSymptomsDataFromFile (String filepath) throws Exception;
 	
 	
+
 	/**
 	 *  Write in a result file the final analyze with specified order. 
 	 *  
-	 * @param resultPath a full or partial path to a create a the file with result, a map with counted symptoms, the ordering option
+	 * @param resultPath  a full or partial path to a create a the file with result,  the ordering option
+	 * @param symptoms  a map with counted symptoms
+	 * @param option  the option to configure the symptoms sort 
+	 * @throws WriteLineException 
+	 * @throws CreateFileException 
 	 * @throws IOException if the result path is incorrect & RuntimeException if the current line can't be written
 	 */
 	public void writeSymptomsCount(String resultPath, Map<String, Integer> symptoms, OrderingOption option) throws Exception ;
